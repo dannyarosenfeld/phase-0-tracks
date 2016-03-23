00bfc7591696addfcf9b0c09f69e44ce02abc75b
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-03-22 21:10:59 -0700

    ask questions about data and use gets chomp

---
+++ ruby/hamsters.rb
@@ -0,0 +1,21 @@
+puts ("what's the hamsters name?")
+name = gets.chomp
+
+
+puts ("How loud is the hamster from 1-10?")
+
+how_loud = gets.chomp
+how_loud.to_i
+
+puts ("what's the fur color of hamster?")
+fur = gets.chomp
+
+puts ("good candidate for adoption(yes, no)?")
+yes_or_no = gets.chomp
+yes = true
+no = false
+
+
+
+puts ("How old is the hamster?")
+age = gets.chomp
