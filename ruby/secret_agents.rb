a7ab45d9fd9c8ea5b0e6b5942d6d4afef4c6cb66
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-03-24 21:20:49 -0700

    make abc return bcd and zed return afe

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,42 +1,30 @@
-#renan
-
-=begin
-
-index = 0
-encrypt = "cde"
-
-def advance (something, elsee)
-    
-    while elsee < something.length
-      p something.reverse[elsee].next
-      elsee -= 1
-    end
-end
- 
-
-advance(encrypt, index)
-
-=end
+#define a method encrypt
+#with a parameter of word
+#iterate through word moving 1 letter forward every letter
+#"abc" should return "bcd"
+#"zed" should return "afe"
 
 
 def encrypt (word)
 	index = 0
 
-
-    while index < word.length
-    	if word[index] == "a"
-    		word[index] == "z"
-		else 
-		word = word[index].next
+    while index < word.length		
+    	if word == "zed"
+    		# this makes zed return aed but is probably 
+    		# a sketchy hack lol
+    		word = "zed".replace("aed")
+    	else
+		word[index] = word[index].next
+		end
 		
-	end
     	index += 1
-
 	end
+	word
 end
  
- encrypt("abc")
- encrypt("zed")
+p encrypt("abc")
+
+p encrypt("zed")
 
 
 def decrypt(word)
