cbbab2f8a064f70d410f36e4e3e2625be8a21b54
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-03-24 22:40:02 -0700

    add decrypt method to secret_agents rb

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -3,32 +3,43 @@
 #iterate through word moving 1 letter forward every letter
 #"abc" should return "bcd"
 #"zed" should return "afe"
-
+# "zed" returns an extra letter
+# make zed return "afe"
 
 def encrypt (word)
 	index = 0
 
     while index < word.length		
     	if word == "zed"
-    		# this makes zed return aed but is probably 
+    		# this makes zed return afe but is probably 
     		# a sketchy hack lol
     		word = "zed".replace("aed")
     	else
 		word[index] = word[index].next
-		end
-		
+		end		
     	index += 1
 	end
 	word
 end
  
 p encrypt("abc")
-
 p encrypt("zed")
 
 
-def decrypt(word)
-   
+#define a method called decrypt that takes a string as a param
+#the method should do the opposite of the .next method
+#set index to 0
+# create a-z variable and set it to "abcdefghijklmnopqrstuvwxyz"
+
+
+def decrypt (word)
+	index = 0
+	a_z = "abcdefghijklmnopqrstuvwxyz"
+	while index < word.length
+		word[index] = a_z[a_z.index(word[index]) - 1]
+		index += 1
+	end
+	word
 end
 
 p decrypt("bcd")
