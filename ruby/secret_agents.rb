4588124883134e083b460e1260f95e011d59ba9f
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-03-27 15:38:04 -0700

    fix spacing error with decrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,61 +1,4 @@
 
-=begin
-#"abc" should return "bcd"
-#"zed" should return "afe"
-# "zed" returns an extra letter
-# make zed return "afe"
-
-#define a method encrypt
-#with a parameter of word
-
-def encrypt (word)
-	#set index to 0
-	index = 0
-	#iterate through word moving 1 letter forward every letter
-    while index < word.length	
-
-    	if word == "zed"
-    		# this makes zed return afe but is probably 
-    		# a sketchy hack lol
-    		word = "zed".replace("aed")
-
-    	else
-
-		word[index] = word[index].next
-
-		end		
-    	index += 1
-	end
-	word
-end
- 
-p encrypt("abc")
-p encrypt("zed")
-
-
-#define a method called decrypt that takes a string as a param
-#the method should do the opposite of the .next method
-#set index to 0
-# create a-z variable and set it to "abcdefghijklmnopqrstuvwxyz"
-
-
-def decrypt (word)
-	index = 0
-	a_z = "abcdefghijklmnopqrstuvwxyz"
-	while index < word.length
-		word[index] = a_z[a_z.index(word[index]) - 1]
-		index += 1
-	end
-	word
-end
-
-p decrypt("bcd")
-p decrypt("afe")
-p decrypt("abc")
-p decrypt("wxy")
-=end
-
-
 
 #define a method encrypt
 #with a parameter of input
@@ -100,19 +43,23 @@ end
 #create alphabet variable
 #loop through word moving 1 letter backwards every letter
 #create variable each_letter and set it to
-#the alphabet with .index(the current letter of input)
-#subtract 1 from the alphabet letter number a_to_z[each_letter - 1]
+#the alphabet.index(the current letter of input)
+#subtract 1 from the alphabet letter number each time a_to_z[each_letter - 1]
 #add that to the empty string
 #print the string
 
+#add space 
  def decrypt (input)
  	index = 0
  	string = ""
- 	a_to_z = "abcdefghijklmnopqrstuvwxyz"
+ 	a_to_z = "abcdefghijklmnopqrstuvwxyz !"
  	while index < input.length
- 		each_letter = a_to_z.index(input[index])
-
+ 		if input[index] == "a"
+ 			string += "z"
+ 		else
+ 		each_letter = a_to_z.index(input[index].downcase)
  		string += a_to_z[each_letter - 1]
+ 		end
  		index += 1
  	end
  	p string
@@ -122,3 +69,7 @@ end
 decrypt("bcd")
 decrypt("afe")
 
+
+# nested method here
+
+decrypt(encrypt("The duck flies at midnight"))
