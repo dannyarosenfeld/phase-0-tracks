2028e1a500505a47e5d66ef01ceedb02ad6a2849
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-03-29 19:13:03 -0700

    add method that returns array with three values

---
+++ ruby/arrays_drill.rb
@@ -0,0 +1,35 @@
+puts "Release 2 do the thing"
+
+def build_array(a, b, c)
+blah = [a, b, c]
+
+
+end
+
+p build_array("haha", 67, false)
+
+
+
+p my_array = [1,2,3,4,5]
+
+my_array.delete_at(2)
+
+p my_array
+
+my_array.insert(2, "blah")
+
+p my_array
+
+my_array.shift
+
+p my_array
+
+true_includes = my_array.include?(2)
+
+p "my array contains the number 2 is #{true_includes}" 
+
+another = [1,2,3]
+
+combined = another + my_array
+
+p combined
