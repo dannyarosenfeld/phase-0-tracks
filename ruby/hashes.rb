c3eb17b18b8b570cdd1b7e4763b9b437f5801c41
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-03-31 15:45:38 -0700

    inital commit with psuedocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,24 @@
+# use puts and gets chomp to prompt the 
+# the client for their name, age, number of children, decor theme, and so on
+# set name and decor theme to strings
+#set age, num children to numbers
+# make this create a hash and return values entered to screen after all is answered
+# prompt user to update a key and run program one more time
+# use method to turn string to symbol
+design_hash = {}
+
+puts "give name"
+name = gets.chomp
+
+puts "give age"
+age = gets.chomp
+
+puts "# of children"
+num_children = gets.chomp
+
+puts "decor theme"
+decor = gets.chomp
+
+puts "phone number"
+phone_num = gets.chomp
+
