35cd7119989f30f2506abedc275a686b33c83f71
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-03-31 18:28:09 -0700

    add methods for iterating through arrays and hashes

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -37,9 +37,53 @@ famous_chefs.each do |chef_key, chef_string|
 end
 p famous_chefs
 
+num_ary = [1,2,3,4,5,6,7,8,9]
+
+num_hash = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8
+}
 
 
+puts" delete if method"
+puts "***********************"
+p num_ary
+num_ary.delete_if {|num| num < 3}
+p num_ary
 
 
+p num_hash
+num_hash.delete_if {|word, num| num < 3 }
+p num_hash
+
+
+puts"keep if method"
+puts "***********************"
+p num_ary
+num_ary.keep_if {|num| num < 7}
+p num_ary
+
+
+p num_hash
+num_hash.keep_if {|word, num| num < 7 }
+p num_hash
+
+
+puts"select method"
+puts "***********************"
+p num_ary
+num_ary.select! {|num| num%3 == 0}
+p num_ary
+
+
+p num_hash
+num_hash.select! {|word, num| num%3 == 0}
+p num_hash
 
 
