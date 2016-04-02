e907fcc3c546bd8028582ad928dbc1c180319379
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-04-02 15:49:31 -0700

    initial commit for nested data

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,15 +1,37 @@
-house = {
-	'Living Room' => [
-	  'sofa',
-	  'chandelier',
-	  'cat'
-	  ],
-	'Kitchen' => [
-		'table',
-		'fridge'
-		]
-}
+24_hour_fitness = {
+	
+	Front_Desk: {
+		Size: "small",
+		Sanitation_Level: "high",
+		Employees: {
+			Customer_Service: 3,
+			Cleaners: 0
+		}
+		Towels: {
+			Clean: 25,
+			Dirty: 0
+		}
 
-p house
-house['Living Room'].insert(2, 'dog')
-p house
+		Equipment: [
+			"finger scanner",
+			"Number input
+		]
+
+		
+
+	}
+
+	Locker_Room:
+
+	Spinning_Room:
+
+	Free_Weight_Area:
+
+	Machine_Area:
+
+
+
+
+
+
+}
