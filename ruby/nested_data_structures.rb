4631874f7a86ac63608de82188987baecd83bf4a
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-04-02 16:13:12 -0700

    complete nested data structure

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,4 +1,4 @@
-24_hour_fitness = {
+hour_fitness = {
 	
 	Front_Desk: {
 		Size: "small",
@@ -6,32 +6,84 @@
 		Employees: {
 			Customer_Service: 3,
 			Cleaners: 0
-		}
+		},
+
 		Towels: {
 			Clean: 25,
 			Dirty: 0
-		}
+		},
 
 		Equipment: [
 			"finger scanner",
-			"Number input
+			"Code Keypad Machine"
+		]
+	},
+
+	Locker_Room: {
+		Size: "medium",
+		Sanitation_Level: "medium",
+		Employees: {
+			Customer_Service: 0,
+			Cleaners: 3
+		},
+
+		Towels: {
+			Clean: 5,
+			Dirty: 8
+		},
+
+		Equipment: [
+			"Lockers",
+			"Combinations Locks"
 		]
 
-		
+	},
 
+	Spinning_Room: {
+		Size: "medium",
+		Sanitation_Level: "sweaty",
+		Employees: {
+			Customer_Service: 0,
+			Cleaners: 2
+		},
+
+		Towels: {
+			Clean: 3,
+			Dirty: 10
+		},
+
+		Equipment: [
+			"Spinning bike machines",
+			"Light freeweights"
+		]
+	},
+
+	Machine_Area: {
+		Size: "Large",
+		Sanitation_Level: "sweaty",
+		Employees: {
+			Customer_Service: 3,
+			Cleaners: 7
+		},
+
+		Towels: {
+			Clean: 3,
+			Dirty: 25
+		},
+
+		Equipment: [
+			"Weight machines",
+			"Light freeweights",
+			"TRX",
+			"Heavy freeweights"
+		]
 	}
-
-	Locker_Room:
-
-	Spinning_Room:
-
-	Free_Weight_Area:
-
-	Machine_Area:
-
-
-
-
-
-
 }
+
+
+
+p hour_fitness
+puts "**********************"
+puts "Push all this stuff"
+	hour_fitness[:Front_Desk].push("blah")
+p hour_fitness
