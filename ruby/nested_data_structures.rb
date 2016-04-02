a12c9eb8db703b1cfa3e35a4eb256a70f8ad5fa7
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-04-02 15:00:14 -0700

    practice with nested data structures initial commit

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,15 @@
+house = {
+	'Living Room' => [
+	  'sofa',
+	  'chandelier',
+	  'cat'
+	  ],
+	'Kitchen' => [
+		'table',
+		'fridge'
+		]
+}
+
+p house
+house['Living Room'].insert(2, 'dog')
+p house
