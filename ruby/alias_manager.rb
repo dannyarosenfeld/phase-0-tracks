76805e8755f9292da9c8330ac1fc7ad18d455804
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-04-02 21:05:34 -0700

    define a couple methods for aeiou and consonants

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,54 @@
+#write a method that loops through every consonant letter of the alphabet
+#and returns the next consonant
+
+#write a method that loops through every vowel
+#and returns the next vowel
+
+def consonants
+	alphabet = "bcdfghjklmnpqrstvwxyz"
+	index = 0
+	string = ""
+	while index < alphabet.length	
+		letters = alphabet[index].next
+		index += 1
+		string += letters
+		
+	end
+	string
+end
+
+
+def vowel(letter)
+alphabet = "aeiou"
+current_letter = alphabet.index(letter)
+alphabet[current_letter + 1]
+end
+
+
+def decrypt(word)
+	index = 0
+	string = ""
+
+	while index < word.length
+
+		char = word[index]
+		index += 1
+		string += vowel(char)
+
+	end
+	string
+
+
+end
+
+
+
+puts decrypt("aeiou")
+
+
+
+
+
+
+
+
