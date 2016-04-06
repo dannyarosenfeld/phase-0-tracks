98f17c055b350e3e2cf4debbb765d568f93eaa9d
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-04-05 20:07:13 -0700

    add class basketball with 3 methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,34 +1,71 @@
-#Release 0
-class Puppy
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+##Release 0
+#class Puppy
 
-  def speak(number)
-    number.times do |x|
-    puts "Woof!"
-    end
+#    def initialize 
+#      puts "Initializing new puppy instance ..."
+#    end
+#  
+#  def fetch(toy)
+#    puts "I brought back the #{toy}!"
+#    toy
+#  end
+#
+#  def speak(number)
+#    number.times do |x|
+#    puts "Woof!"
+#    end
+#  end
+#  
+#  def roll_over(command)
+#    puts "roll over #{command}"
+#  end
+#  
+#  def dog_years(age)
+#    age.to_i * 7
+#  end
+#  
+#  def shake(command)
+#    puts "shake #{command}"
+#  end
+#  
+#end
+#
+#puppy1 = Puppy.new
+##Driver code
+#puppy1.fetch("ball")
+#p puppy1.speak(5)
+#p puppy1.roll_over("Doggy")
+#p puppy1.dog_years(2)
+#p puppy1.shake("Doggy")
+
+#Release 2
+#Creating new class
+class Basketball
+  def initialize 
+    puts "you created a Basketball instance!!"
   end
   
-  def roll_over(command)
-    puts "roll over #{command}"
+  def dunk
+    puts "And he throws it down!!"
   end
   
-  def dog_years(age)
-    age.to_i * 7
+  def three_pointer
+    puts "splash bro's in the building"
   end
   
-  def shake(command)
-    puts "shake #{command}"
-  end
   
 end
+#Container for instances
+basketball_array = []
 
-puppy1 = Puppy.new
-#Driver code
-puppy1.fetch("ball")
-p puppy1.speak(5)
-p puppy1.roll_over("Doggy")
-p puppy1.dog_years(2)
-p puppy1.shake("Doggy")
+50.times do
+player1 = Basketball.new
+player1
+basketball_array << player1
+end
+puts basketball_array.length
+#Iterating everything inside of the basketball array
+basketball_array.each do |instances| 
+  instances.dunk
+  instances.three_pointer
+end
