7b017fb264bd8dd0a1c7ae1ff797c0a191f3ce5f
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-04-06 20:53:18 -0700

    put the icecream value last problem

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,10 +1,11 @@
+
 def create_items(item)
- 	grcy_array = item.split(" ")
- 	grcy_hash = {}
- 	grcy_array.map do |item|
- 	grcy_hash[item] = 0
- 	end
- 	grcy_hash
+    grcy_array = item.split(" ")
+    grcy_hash = {}
+    grcy_array.map do |item|
+    grcy_hash[item] = 0
+    end
+    grcy_hash
 end
 
 p create_items("carrots potatoes tomatoes straweberrits")
@@ -19,9 +20,9 @@ p create_items("carrots potatoes tomatoes straweberrits")
 new_grcy_hash = create_items("carrots potatoes tomatoes straweberrits")
 
 def add_item(grcy_hash, item, qty)
- grcy_hash
- grcy_hash[item.to_sym] = qty.to_i
- grcy_hash
+grcy_hash
+grcy_hash[item.to_sym] = qty.to_i
+grcy_hash
 end
 
 p add_item(new_grcy_hash, "carrtos", 2)
@@ -37,8 +38,8 @@ p add_item(new_grcy_hash, "carrtos", 2)
 new_grcy_hash2 = add_item(new_grcy_hash, "carrtos", 2)
 
 def remove_item(grcy_hash, item)
- grcy_hash.delete(item)
- grcy_hash
+grcy_hash.delete(item.to_sym)
+grcy_hash
 end
 
 p remove_item(new_grcy_hash2, "potatoes")
@@ -58,9 +59,9 @@ p remove_item(new_grcy_hash2, "potatoes")
 new_grcy_hash3 = remove_item(new_grcy_hash2, "potatoes")
 
 def qty_change(grcy_hash, item, qty)
- grcy_hash.delete(item)
- 	grcy_hash[item.to_sym] = qty.to_i
- 	grcy_hash
+grcy_hash.delete(item)
+    grcy_hash[item.to_sym] = qty.to_i
+    grcy_hash
 end
 
 p qty_change(new_grcy_hash3, "tomatoes", 3)
@@ -70,18 +71,26 @@ p qty_change(new_grcy_hash3, "tomatoes", 3)
 # steps:
 #1. new_grcy_hash4 set equal qty_change
 #2. def grcy_list set param grcy_hash
-#3.  use each block param put message includes item and qty   
+#3. use each block param put message includes item and qty  
 # output:
 
 new_grcy_hash4 = qty_change(new_grcy_hash3, "tomatoes", 3)
 
 def grcy_list(grcy_list)
-	grcy_list.each do |item, qty|
-		puts "We need #{qty} #{item}"
-	end
+grcy_list.each do |item, qty|
+    puts "We need #{qty} #{item}"
+end
 end
 
+
+#########
+
+p add_item(new_grcy_hash, "lemonade", 2)
+p add_item(new_grcy_hash, "onion", 1)
+p qty_change(new_grcy_hash3, "tomatoes", 3)
+p add_item(new_grcy_hash, "ice cream", 4)
+
+p remove_item(new_grcy_hash2, "lemonade")
+p qty_change(new_grcy_hash3, "ice cream", 1)
+
 p grcy_list(new_grcy_hash4)
-
-
-
