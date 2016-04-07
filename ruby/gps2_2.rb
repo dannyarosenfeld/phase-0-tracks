d9ffa48f7e5ecccf556d3e1cb01ff576b0228f80
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-04-06 20:38:15 -0700

    add puts method to make hash pretty

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -45,14 +45,43 @@ p remove_item(new_grcy_hash2, "potatoes")
 
 
 # Method to update the quantity of an item
-# input:
+# input: 
 # steps:
+# step1 create new grocery hash 3 set equal to current state of hash
+# step2 define method qty_change with params hash, item, and qty
+# step3 call grcy hash
+# step4 call grcy_hash with key item.to_sym make equal qty.to_i
+# step5 call grcry hash
+#test method
 # output:
+
+new_grcy_hash3 = remove_item(new_grcy_hash2, "potatoes")
+
+def qty_change(grcy_hash, item, qty)
+ grcy_hash.delete(item)
+ 	grcy_hash[item.to_sym] = qty.to_i
+ 	grcy_hash
+end
+
+p qty_change(new_grcy_hash3, "tomatoes", 3)
 
 # Method to print a list and make it look pretty
 # input:
 # steps:
+#1. new_grcy_hash4 set equal qty_change
+#2. def grcy_list set param grcy_hash
+#3.  use each block param put message includes item and qty   
 # output:
+
+new_grcy_hash4 = qty_change(new_grcy_hash3, "tomatoes", 3)
+
+def grcy_list(grcy_list)
+	grcy_list.each do |item, qty|
+		puts "We need #{qty} #{item}"
+	end
+end
+
+p grcy_list(new_grcy_hash4)
 
 
 
