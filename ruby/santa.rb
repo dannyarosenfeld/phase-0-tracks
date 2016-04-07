03e5df145289a1bf6e2a16a8315c6b51806f2297
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-04-06 23:22:44 -0700

    iterate through hash for genders and ethnics

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,8 +1,6 @@
 class Santa
-def initialize
-	puts "Initializing Santa instance ..."
-	@gender = gender
-	@ethnicity = ethnicity
+def initialize(gender, ethn)
+	@gen_eth = [gender, ethn]
 	@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 	@age = 0
 end
@@ -15,11 +13,22 @@ def eat_milk_and_cookies(type)
 	puts "That was a good #{type}!" 
 
 end
+end
 
+santas = {}
+genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 
+ethnic = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+genders.length.times do |i|
+santas[genders[i]] = ethnic[i]
 end
 
 
-santa1 = Santa.new
+
+santa1 = Santa.new("Ambiguous", "Asian")
+
+p santas
+
 santa1.speak
 santa1.eat_milk_and_cookies("mint")
