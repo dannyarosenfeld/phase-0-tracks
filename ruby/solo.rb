c3b4634aaa68a415c4dc99928b3d05c59f79a9e1
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-04-08 21:56:32 -0700

    update user interface with attr accessors

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -10,9 +10,9 @@ class SmallDog
 	attr_reader :fur_length, :fur_type 
 	attr_accessor :name, :age_dog_years
 
-	def initialize(name, age_dog_years)
+	def initialize(name, age)
 		@name = name
-		@age_dog_years = age_dog_years * 7
+		@age_dog_years = age * 7
 
 		@length_ary = ["short hair", "medium hair", "insanely long hair", "a pink mohawk", "slimey hair"]
 		@fur_length =  @length_ary[rand(@length_ary.length)]
@@ -26,6 +26,7 @@ class SmallDog
 
 	def pet_dog
 		puts "mmmmm, such #{@fur_length}, and oh so #{@fur_type} for a dog named #{@name}"
+		puts "By the way, #{@name} is also #{@age_dog_years} in dog years! Wow!"
 	end
 
 end
@@ -45,34 +46,42 @@ dog5.pet_dog
 dog6.pet_dog
 p dog1
 =end
+doggies = []
+
 loop do
-puts "Hey there! please input a name of a small dog to create one!"
-answer = gets.chomp.to_s
-puts "how old is #{answer}?"
-age = gets.chomp.to_i
-if age < 10
-	dog = SmallDog.new(answer, age)
+	puts "Hey there! please input a name of a small dog to create one!"
+	answer = gets.chomp.to_s
+	break if answer == "done"
+	puts "how old is #{answer}?"
+	age = gets.chomp.to_i
+	break if age == "done"
 
-	puts "would you like to pet #{answer}? y/n"
-	y_n = gets.chomp.to_s
-	loop do
-		
-		if y_n == "y" 
-		p dog.pet_dog
-		break
-	    elsif y_n == "n"
-	    	p "okay, so you're not an animal lover that's cool"
-	    	break
-	    else
-	    	p "please enter y/n"
-	    	puts "would you like to pet #{answer}? y/n"
-			y_n = gets.chomp.to_s
-	    end
-	end
-else
-puts "please enter age that's less than 10!"
+	if age < 10		
+		dog = SmallDog.new(answer, age)
+		doggies << dog
+		puts "would you like to pet #{answer}? (y/n) come on, #{answer} has #{dog.fur_length}!"
+		y_n = gets.chomp.to_s
+		break if y_n == "done"
+		loop do					
+			if y_n == "y" 
+				p dog.pet_dog				
+				break
+		    elsif y_n == "n"
+		    	puts "okay, so you're not an animal lover that's cool."	
+		    	puts "either that or you're scared of dogs that are #{dog.fur_type} with #{dog.fur_length}"
+		    	puts "because that's exactly how you made #{dog.name} "	    	
+		    	break
+		    else
+		    	p "please enter y/n"
+		    	puts "would you like to pet #{answer}? y/n"
+				y_n = gets.chomp.to_s
+				break if y_n == "done"	
+		    end
+		end
+	else
+		puts "please enter age that's less than 10!"
+	end	
 end
 
-
-
-end
+puts "hope you had fun creating dogs and running the pet method!"
+p doggies
