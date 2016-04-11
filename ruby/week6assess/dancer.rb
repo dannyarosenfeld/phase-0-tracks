484d060f5bd276c8f931e8a7cefbec788c858ebc
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-04-10 18:00:10 -0700

    add custom test to limit array length

--- ruby/week6assess/dancer.rb
+++ ruby/week6assess/dancer.rb
@@ -12,4 +12,41 @@ class Dancer
 	def age
 		@age
 	end
+
+	def pirouette
+		p "*twirls*"
+	end
+
+	def bow
+		p "*bows*"
+	end
+
+
+
+	def queue_dance_with(partner)
+		@partner = partner
+		@ary = @ary.to_a.push @partner
+	
+	end
+
+	def card	
+		@ary
+	end
+
+	def begin_next_dance
+		p "Now dancing with #{@ary[0]}."
+	end
+
+	def limit(num)
+		if @ary.length < num
+			p "card contains less than #{num}"
+		else
+			p "Oh, no! card contains more than #{num}"
+		end
+	end
 end
+
+
+
+
+#chose to limit 
--- ruby/week6assess/dancer_spec.rb
+++ ruby/week6assess/dancer_spec.rb
@@ -15,4 +15,38 @@ describe Dancer do
     dancer.age = 34
     expect(dancer.age).to eq 34
   end
+
+  it "twirls" do
+    expect(dancer.pirouette).to eq "*twirls*"
+  end
+
+  it "bows to a partner" do
+    expect(dancer.bow).to eq "*bows*"
+  end
+
+
+
+
+  it "can queue parters on the dance card" do
+    dancer.queue_dance_with("Mikhail Baryshnikov")
+    expect(dancer.card).to eq ["Mikhail Baryshnikov"]
+    dancer.queue_dance_with("Anna Pavlova")
+    expect(dancer.card).to eq ["Mikhail Baryshnikov", "Anna Pavlova"]
+  end
+
+  it "can start the next dance in the queue" do
+    dancer.queue_dance_with("Mikhail Baryshnikov")
+    dancer.queue_dance_with("Anna Pavlova")
+    expect(dancer.begin_next_dance).to eq "Now dancing with Mikhail Baryshnikov."
+    expect(dancer.card).to eq ["Anna Pavlova"]
+  end
+
+    it "limits how many dancers on the dance card" do
+    dancer.queue_dance_with("Mikhail Baryshnikov")
+    dancer.queue_dance_with("Anna Pavlova")
+    dancer.queue_dance_with("Jimmy")
+    dancer.queue_dance_with("Timmy")
+    expect(dancer.limit(4)).to eq "Oh, no! card contains more than 4"  
+  end
+
 end
