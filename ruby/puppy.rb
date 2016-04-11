352b905b48114a0de003d457564ae9679dac9a6f
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-04-10 23:48:03 -0700

    add characteristics to puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+
+=begin
+class Puppy
+SPECIES ------------------------
+Canine
+
+CHARACTERISTICS ----------------
+Eyelash length: curly
+Friendly: somtimes
+Eye count: 2
+Name: varies
+Color: varies
+
+BEHAVIOR -----------------------
+Jump
+Dig
+Cuddle
+Chew
+end
+=end
