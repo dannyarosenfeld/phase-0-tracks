0085f9ee2c31f31fdc43d9fd0c6508fd6ce2e34e
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-04-13 21:36:15 -0700

    add psuedocode to explore.js

--- js/explore.js
+++ js/explore.js
@@ -1,4 +1,12 @@
-
+/*
+Create function called reverse with word as a parameter
+declare an empty string variable
+make for loop with i equal to the input words length
+as long as i is greater than or eaql to zero print each letter backwards
+add each letter to the empty string
+return the string outside of the for loop
+outside of the function call the function inside of a console log statement
+*/
 
 function reverse(word){
 	var a = '';
