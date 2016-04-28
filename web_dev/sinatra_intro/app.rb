d04af461c0e24baa14402730d292422996d5acdb
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-04-27 17:21:49 -0700

    fix psuedocode for search students get method in sinatra

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -8,20 +8,46 @@ db.results_as_hash = true
 # write a basic GET route
 # add a query parameter
 # GET /
-=begin
+#take query params for name and age
+#return a string with them
+#else return hello you
 get '/' do
+  if params[:name] || params[:age]
   "#{params[:name]} is #{params[:age]} years old."
+else
+  "hello you!"
 end
-=end
 
-get '/:campus' do
-  all_names = db.execute("SELECT * FROM students WHERE campus=?", [params[:campus]]) 
-  string = ""
-  all_names.each do |name|
-     string << "#{name['name']} goes to SF to learn at DBC <br><br>"
-  end
-string
 end
+
+
+#create address string for contact route
+get '/contact' do
+  "1300 McTree Ave, SF"
+end
+#congratulate if name exists addd name
+#if not return good job
+get '/great_job/' do
+  
+  if params[:employee]
+    "Good Job, #{params[:employee]}!"
+    
+  else
+    "Good Job!"
+  end
+end
+
+
+#set route params for adding two numbers
+#set  params as variables num1 and num2
+#return a string saying that they are added and add them
+get '/:num1/plus/:num2' do
+  num1 = params[:num1]
+  num2 = params[:num2]
+  "#{num1} plus #{num2} equals #{num1.to_i + num2.to_i}"
+end
+
+
 # write a GET route with
 # route parameters
 get '/about/:person' do
@@ -35,49 +61,49 @@ end
 
 # write a GET route that retrieves
 # all student data
+#print the output as strings
+#add those strings to the response string
+#use break tags to format data
 get '/students' do
   students = db.execute("SELECT * FROM students")
   response = ""
+
   students.each do |student|
     response << "ID: #{student['id']}<br>"
     response << "Name: #{student['name']}<br>"
     response << "Age: #{student['age']}<br>"
     response << "Campus: #{student['campus']}<br><br>"
   end
-  response
+  students.to_s
+  response.to_s
 end
+
+
+#create campus route parameter
+#iterate through students db where the campus input matches
+#add the matches to a formatted string
+#search students by campus and list as strings
+get '/:campus' do
+  all_names = db.execute("SELECT * FROM students WHERE campus=?", [params[:campus]]) 
+  string = ""
+  all_names.each do |name|
+     string << "#{name['name']} goes to SF to learn at DBC <br><br>"
+  end
+string
+end
+
+
 
 # write a GET route that retrieves
-# a particular student
-
+# a particular student by id
 get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])
-  p student.to_s
- 
-end
-
-get '/contact' do
-  address = "1300 McTree Ave, SF"
-  address.to_s
-end
-
-get '/great_job/' do
-  name = params[:name]
-  if name
-    return "Good Job, #{name}!"
-    return name.to_s
-  else
-    return "Good Job!"
+  str = ""
+  student.each do |student|
+    str << "name: #{student['name']} age: #{student['age']} campus: #{student['campus']}"
   end
+ str
 end
 
-  get '/:num1/plus/:num2' do
-    "hello world"
-    num1 = params[:num1]
-    num2 = params[:num2]
-  
-    "#{num1} plus #{num2} = #{num1.to_i + num2.to_i}"
-
-  end
 
 
